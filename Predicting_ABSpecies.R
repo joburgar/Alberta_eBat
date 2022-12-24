453da544211d0fd5e8423cf20e15bdf6e12e01c9
Joanna Burgar <joburgar@gmail.com>
2022-12-24 09:28:40 -0800

    Small revisions to streamline analysis.

--- Predicting_ABSpecies.R
+++ Predicting_ABSpecies.R
@@ -5,9 +5,9 @@ library(tidyverse)
 library(knitr)
 library(reshape2)
 ########################################################################################
-
-data_in <- read.csv("Sewage_lagoon_all.csv") #call data into R
-
+input_data <- "SewageLagoon"
+data_in <- read.csv(paste0(input_data,"_output.csv")) #call data into R
+head(data_in)
 # subset to parameters from AnaLook
 data_sub <- data_in[c("FileName","Dur.ms","Fmax.kHz","Fmin.kHz","Fmean.kHz","Tk.ms","Fk.kHz","Quality..","Tc.ms","Fc.kHz","S1.OPS","Sc.OPS")]
 
@@ -40,15 +40,16 @@ M1$Filename <- filenames_to_keep ##add filenames back in
 # this bit will need to be changed depending on how people name their files
 M1$Site <- word(M1$Filename, 1, sep=fixed('_'))
 M1$Date <- word(M1$Filename, 2, sep=fixed('_'))
-
-bat_data <- M1
+glimpse(M1)
 
 ## Create output that mimics output from Alberta eBat
 # exception is that counts file removes all calls with <3 pulses rather than attributing to unkown
 # are kept in summary file though
 
-test <- compute_bat_counts_fn(bat_data=bat_data)
+eBat_output <- compute_bat_counts_fn(bat_data=M1)
 
 # need to consider naming convention - does it matter for how it's uploaded into annual report? Might just need the counts and summary bits (i.e., grepl)
-write.csv(test$bat_counts,"Sewage_Lagoon_counts.csv", row.names=F)
-write.csv(test$bat_summary, "Sewage_Lagoon_summary.csv", row.names=F)
+write.csv(eBat_output$bat_counts,paste0(input_data,"_counts.csv"), row.names=F)
+write.csv(eBat_output$bat_summary, paste0(input_data,"_summary.csv"), row.names=F)
+
+rm(data_in, dataM, data_sub, data, M1)
